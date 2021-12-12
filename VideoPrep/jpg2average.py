6300edb75e7371a7a8fb72963982051903805a26
AguirreMoy <aguirremoy@gmail.com>
2021-12-12 01:11:32 -0800

    first json of the led control set

--- VideoPrep/jpg2average.py
+++ VideoPrep/jpg2average.py
@@ -1,15 +1,33 @@
 import numpy as np
 from skimage import data, util, io
 import matplotlib.pyplot as plt
+import glob
+from natsort import natsorted
+import json
+# All files ending with .txt
+img_list = natsorted(glob.glob("C:/Users/Aguir/Documents/GitHub/PiZeroCHristmasTree/VideoPrep/Porter/*.jpg"))
 
-astro = data.astronaut()
-blocks = util.view_as_blocks(astro, (8, 8, 3))
+output_file_path = "C:/Users/Aguir/Documents/GitHub/PiZeroCHristmasTree/VideoPrep/porter.json"
 
-img_data = io.imread()
-print(astro.shape)
-print(blocks.shape)
+data = {}
 
-mean_color = np.mean(blocks, axis=(2, 3, 4))
+NUM_LED = 50
 
-fig, ax = plt.subplots()
-ax.imshow(mean_color.astype(np.uint8))
+
+for count, img_path in enumerate(img_list):
+    img_data = io.imread(img_path)
+    blocks = util.view_as_blocks(img_data, (9, 16, 3))
+    print(img_data.shape)
+    print(blocks.shape)
+
+    mean_color = np.mean(blocks, axis=(2, 3, 4))
+    my_dict = {}
+    for i in range(NUM_LED):
+        my_dict["LED" + str(i)] = mean_color.astype(np.uint8)[7][7].tolist()
+
+#    fig, ax = plt.subplots()
+#    ax.imshow(mean_color.astype(np.uint8))
+
+    data['frame' + str(count)] = my_dict
+with open(output_file_path, 'w') as fout:
+    json.dump(data, fout, sort_keys=True, indent=4)
