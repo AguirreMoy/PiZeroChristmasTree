4077897aa19208bb694c1cf3a7ad6c08eaceb2a4
AguirreMoy <aguirremoy@gmail.com>
2021-12-12 19:00:46 -0800

    Porter full image test

--- VideoPrep/play_json_strip.py
+++ VideoPrep/play_json_strip.py
@@ -2,7 +2,8 @@ import json
 import time
 starttime = time.time()
 
-time_delay = 1.0
+time_delay = 0.1
+
 
 
 file_name = "C:/Users/Aguir/Documents/GitHub/PiZeroChristmasTree/VideoPrep/porter.json"
@@ -12,8 +13,8 @@ playData = json.load(f)
 for i in playData:
     frame_data = playData[i]
     for count, i in enumerate(frame_data):
-        color_data = frame_data[i]
-        print(i, frame_data[i])
-#        strip.setPixelColor(count, Color(color_data[1],color_data[0], color_data[2]))
-#    strip.show()
+        # color_data = frame_data[i]
+        # print(i, frame_data[i])
+        strip.setPixelColor(count, Color(color_data[1],color_data[0], color_data[2]))
+    strip.show()
     time.sleep(time_delay - ((time.time() - starttime) % time_delay))
--- VideoPrep/video2jpg.py
+++ VideoPrep/video2jpg.py
@@ -23,4 +23,4 @@ if __name__=="__main__":
     args = a.parse_args()
     print(args)
     #extractImages(args.pathIn, args.pathOut)
-    extractImages("C:/Users/Aguir/Documents/GitHub/PiZeroChristmasTree/VideoPrep/zed_doit.mp4", "C:/Users/Aguir/Documents/GitHub/PiZeroChristmasTree/VideoPrep/ZedDoIt/")
+    extractImages("C:/Users/Aguir/Documents/GitHub/PiZeroChristmasTree/VideoPrep/porter_intro_sad_machine.mp4", "C:/Users/Aguir/Documents/GitHub/PiZeroChristmasTree/VideoPrep/Porter/")
